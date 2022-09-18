50553ba53eeae39606f1a92cb113a11adaf5d27c
sendy452 <sendyivenyulian452@gmail.com>
2022-09-18 09:35:16 +0700

    Revert "Merge pull request #1 from sendy452/sprint2"
    This reverts commit 24d50e76756fe098bbba997da2a4abb2455acf57, reversing
    changes made to c193f47ea7e47a533958d6ed9d8c6cb101b445f5.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,42 +5,29 @@ namespace App\Models;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements JWTSubject, AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
-    
-    protected $table = 'm_user';
+    use Authenticatable, Authorizable, HasFactory;
+
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = [
-        'email', 'name'
+        'name', 'email',
     ];
 
     /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
+     * The attributes excluded from the model's JSON form.
      *
-     * @return mixed
+     * @var string[]
      */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-
+    protected $hidden = [
+        'password',
+    ];
 }
